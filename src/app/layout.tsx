d31fcd796b74201155ac1a17e2c55a5c6845cdcc
lucash0rta <lucashorta2001@gmail.com>
2025-10-10 14:43:26 +1100

    Enable SpeedInsights and Analytics in layout
    Added <SpeedInsights /> and <Analytics /> components to the RootLayout for improved monitoring and analytics. Also removed the rule disabling '@typescript-eslint/no-unused-vars' from ESLint config to re-enforce unused variable checks.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
